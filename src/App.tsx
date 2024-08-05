7d8110b72da92c1c10be87446d1e9b998c9ad991
Gautam Juneja <g.juneja@iitg.ac.in>
2024-08-05 12:05:46 +0530

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import './global.css';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+
+    <main className='flex h-screen'>
+      <Routes>
+        {/* public routes */}
+        <Route path="/sign-in" element={<SigninForm />} />
+        {/* <Route path="/about" element={<About />} /> */}
+
+        {/* private routes */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 // import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <App/>
+
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+    
+
 );
