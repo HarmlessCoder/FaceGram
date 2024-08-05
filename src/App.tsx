d927540cd31fc42e799cc68fc3d379e0145c8ead
Gautam Juneja <g.juneja@iitg.ac.in>
2024-08-05 17:12:23 +0530

    sign up form UI done

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './global.css';
-import { Routes, Route } from 'react-router-dom';
+import "./global.css";
+import { Routes, Route } from "react-router-dom";
 
-import SigninForm from './_auth/forms/SigninForm';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
 
-import { Home  } from './_root/pages';
-import RootLayout from './_root/RootLayout';
+import { Home } from "./_root/pages";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "./components/ui/toaster";
 
 const App = () => {
   return (
@@ -14,7 +15,7 @@ const App = () => {
     //   Hello world!
     // </h1>
 
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
       <Routes>
         {/* public routes */}
         <Route element={<AuthLayout />}>
@@ -28,8 +29,9 @@ const App = () => {
           <Route index element={<Home />} />
         </Route>
       </Routes>
+      <Toaster />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
