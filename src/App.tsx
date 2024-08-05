6e79104b5111da4bb079040a8c91850a0255ac19
Gautam Juneja <g.juneja@iitg.ac.in>
2024-08-05 15:34:29 +0530

    routing and assets added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import './global.css';
 import { Routes, Route } from 'react-router-dom';
 
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import AuthLayout from './_auth/AuthLayout';
+
+import { Home  } from './_root/pages';
+import RootLayout from './_root/RootLayout';
+
 const App = () => {
   return (
     // <h1 className="text-3xl font-bold underline">
@@ -10,11 +17,16 @@ const App = () => {
     <main className='flex h-screen'>
       <Routes>
         {/* public routes */}
-        <Route path="/sign-in" element={<SigninForm />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+          {/* <Route path="/about" element={<About />} /> */}
+        </Route>
 
         {/* private routes */}
-        <Route index element={<Home />} />
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </main>
   )
